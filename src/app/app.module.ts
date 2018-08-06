fcd2e8b6b5ee202fbd3c10273abe5fe4e83e8053
Shane Morris <shane.morris@partnersandnapier.com>
2018-08-06 13:54:07 -0400

    Added angular material, datepicker, fixed some css issues on mobile.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
@@ -12,6 +11,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { SelectSearchableModule } from 'ionic-select-searchable';
 import { AuthServiceProvider } from '../providers/auth-service/auth-service';
 import { LoginPage } from '../pages/login/login';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule, MatNativeDateModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,10 @@ import { LoginPage } from '../pages/login/login';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    SelectSearchableModule
+    SelectSearchableModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
